e7d5e39365575cd86bf6979e4adb7b8f8ea8070c
jasurbeknurboyev <998946972365>
2022-12-21 21:37:04 +0500

    Initial setup

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+Release history
+
+## 0.1.0 - 21.12.2022
+ - Initial setup
---
+++ tests/test_hello_world.py
@@ -0,0 +1,9 @@
+"""Sample test."""
+
+
+class TestHello:
+    """Sample test class."""
+
+    def test_hello(self):
+        """Sample test."""
+        assert len("hello") == 5
